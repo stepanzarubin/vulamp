e910cb8f783f6a46d5e72751b2b923a916b145e8
Stepan Zarubin <stepan.zarubin@gmail.com>
2017-11-29 16:57:02 +0200

    sqlite3

--- sh/bootstraps/php/5.5.sh
+++ sh/bootstraps/php/5.5.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt-get -y install php5 php5-curl php5-gd php5-mcrypt php5-mysql php5-imap php5-json php5-intl
+sudo apt-get -y install php5 php5-curl php5-gd php5-mcrypt php5-mysql php5-sqlite3 php5-imap php5-json php5-intl
 sudo cp /usr/share/php5/php.ini-development /etc/php5/apache2/php.ini
 
 #enable modules
